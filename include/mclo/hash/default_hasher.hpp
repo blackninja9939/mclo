52c560b81dc02b0de556306cdac0749f5ad9ecc4
mclohessy <mclohessy@microsoft.com>
2025-01-04 17:58:46 +0100

    Fix default hasher include

--- include/mclo/hash/default_hasher.hpp
+++ include/mclo/hash/default_hasher.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
-#include "mclo/hash/fnv1a_hasher.hpp"
+#include "mclo/preprocessor/expand.hpp"
+#include "mclo/preprocessor/stringify.hpp"
+
+#ifndef MCLO_CONFIG_DEFAULT_HASHER
+#define MCLO_CONFIG_DEFAULT_HASHER fnv1a_hasher
+#endif
+
+#include MCLO_STRINGIFY( MCLO_EXPAND( MCLO_CONFIG_DEFAULT_HASHER ).hpp )
 
 namespace mclo
 {
-#ifdef MCLO_CONFIG_DEFAULT_HASHER
 	using default_hasher = MCLO_CONFIG_DEFAULT_HASHER;
-#else
-	using default_hasher = fnv1a_hasher;
-#endif
 }
---
+++ include/mclo/preprocessor/expand.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define MCLO_EXPAND( X ) X
