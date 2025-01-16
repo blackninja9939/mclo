dca22023af921473ae457a8da9cd415a26162dcc
blackninja9939 <blackninja9939@users.noreply.github.com>
2025-01-16 02:16:27 +0100

    Add numeric vector initial draft

---
+++ include/mclo/numeric/vector.hpp
@@ -0,0 +1,478 @@
+#pragma once
+
+#include "mclo/functional/bind.hpp"
+#include "mclo/utility/array.hpp"
+
+#include <array>
+#include <cmath>
+#include <functional>
+#include <span>
+
+namespace mclo
+{
+	template <typename T, std::size_t N>
+	class vec_base
+	{
+	public:
+		using value_type = T;
+
+		constexpr vec_base() noexcept = default;
+
+		explicit vec_base( const T value ) noexcept
+			: m_data{ broadcast_array<N>( value ) }
+		{
+		}
+
+		template <typename... Ts>
+			requires( sizeof...( Ts ) == N )
+		explicit( ( !std::convertible_to<Ts, T> || ... ) ) vec_base( Ts... values ) noexcept
+			: m_data{ static_cast<T>( values )... }
+		{
+		}
+
+		explicit vec_base( const std::span<const T, N> values ) noexcept
+			: m_data{ to_array( values ) }
+		{
+		}
+
+		// Data access
+		[[nodiscard]] constexpr T& operator[]( const std::size_t index ) noexcept
+		{
+			return m_data[ index ];
+		}
+		[[nodiscard]] constexpr T operator[]( const std::size_t index ) const noexcept
+		{
+			return m_data[ index ];
+		}
+
+		[[nodiscard]] constexpr T* data() noexcept
+		{
+			return m_data.data();
+		}
+		[[nodiscard]] constexpr const T* data() const noexcept
+		{
+			return m_data.data();
+		}
+
+		[[nodiscard]] static constexpr std::size_t size() noexcept
+		{
+			return N;
+		}
+
+		[[nodiscard]] constexpr operator std::span<T, N>() noexcept
+		{
+			return std::span( m_data );
+		}
+		[[nodiscard]] constexpr operator std::span<const T, N>() const noexcept
+		{
+			return std::span( m_data );
+		}
+
+#define MCLO_VEC_ACCESSOR( NAME, INDEX )                                                                               \
+	T& NAME() noexcept                                                                                                 \
+		requires( N > INDEX )                                                                                          \
+	{                                                                                                                  \
+		return this->operator[]( INDEX );                                                                              \
+	}                                                                                                                  \
+	T NAME() const noexcept                                                                                            \
+		requires( N > INDEX )                                                                                          \
+	{                                                                                                                  \
+		return this->operator[]( INDEX );                                                                              \
+	}
+
+		MCLO_VEC_ACCESSOR( x, 0 )
+		MCLO_VEC_ACCESSOR( y, 1 )
+		MCLO_VEC_ACCESSOR( z, 2 )
+		MCLO_VEC_ACCESSOR( w, 3 )
+
+#undef MCLO_VEC_ACCESSOR
+
+		// Generic element-wise operations
+		template <typename Func>
+		[[nodiscard]] constexpr vec_base map( Func func ) const noexcept
+		{
+			return map_internal( func, std::make_index_sequence<N>{} );
+		}
+
+		template <typename Func>
+		[[nodiscard]] constexpr vec_base map_with( Func func, const vec_base& other ) const noexcept
+		{
+			return map_with_internal( func, other, std::make_index_sequence<N>{} );
+		}
+
+		template <typename Func>
+		[[nodiscard]] constexpr T fold_left( Func func, T initial = T( 0 ) ) const noexcept
+		{
+			return fold_left_internal( func, initial, std::make_index_sequence<N>{} );
+		}
+
+		template <typename Func>
+		[[nodiscard]] constexpr T fold_right( Func func, T initial = T( 0 ) ) const noexcept
+		{
+			return fold_right_internal( func, initial, std::make_index_sequence<N>{} );
+		}
+
+		// Element wise folds
+		[[nodiscard]] constexpr T sum() const noexcept
+		{
+			return fold_left( std::plus{} );
+		}
+
+		[[nodiscard]] constexpr T product() const noexcept
+		{
+			return fold_left( std::multiplies{}, T( 1 ) );
+		}
+
+		[[nodiscard]] constexpr T mean() const noexcept
+		{
+			return sum() / N;
+		}
+
+		// Vector arithmetic operators
+		[[nodiscard]] constexpr vec_base operator+( const vec_base& other ) const noexcept
+		{
+			return map_with( std::plus{}, other );
+		}
+
+		constexpr vec_base& operator+=( const vec_base& other ) noexcept
+		{
+			*this = *this + other;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator-( const vec_base& other ) const noexcept
+		{
+			return map_with( std::minus{}, other );
+		}
+
+		constexpr vec_base& operator-=( const vec_base& other ) noexcept
+		{
+			*this = *this - other;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator*( const vec_base& other ) const noexcept
+		{
+			return map_with( std::multiplies{}, other );
+		}
+
+		constexpr vec_base& operator*=( const vec_base& other ) noexcept
+		{
+			*this = *this * other;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator/( const vec_base& other ) const noexcept
+		{
+			return map_with( std::divides{}, other );
+		}
+
+		constexpr vec_base& operator/=( const vec_base& other ) noexcept
+		{
+			*this = *this / other;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator-() const noexcept
+		{
+			return map( std::negate{} );
+		}
+
+		// Scalr arithmetic operators
+		[[nodiscard]] constexpr vec_base operator+( const T scalar ) const noexcept
+		{
+			return map( std::plus{}, scalar );
+		}
+
+		constexpr vec_base& operator+=( const T scalar ) noexcept
+		{
+			*this = *this + scalar;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator-( const T scalar ) const noexcept
+		{
+			return map( bind_back( std::minus{}, scalar ) );
+		}
+
+		constexpr vec_base& operator-=( const T scalar ) noexcept
+		{
+			*this = *this - scalar;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator*( const T scalar ) const noexcept
+		{
+			return map( bind_back( std::multiplies{}, scalar ) );
+		}
+
+		constexpr vec_base& operator*=( const T scalar ) noexcept
+		{
+			*this = *this * scalar;
+			return static_cast<vec_base&>( *this );
+		}
+
+		[[nodiscard]] constexpr vec_base operator/( const T scalar ) const noexcept
+		{
+			return map( bind_back( std::divides{}, scalar ) );
+		}
+
+		constexpr vec_base& operator/=( const T scalar ) noexcept
+		{
+			*this = *this / scalar;
+			return static_cast<vec_base&>( *this );
+		}
+
+		// Basic operations
+		[[nodiscard]] constexpr vec_base abs() const noexcept
+		{
+			using std::abs;
+			return map( []( const T value ) { return static_cast<T>( abs( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base reciprocal() const noexcept
+		{
+			return map( []( const T value ) { return T( 1 ) / value; } );
+		}
+
+		// Rounding operations
+		[[nodiscard]] constexpr vec_base floor() const noexcept
+		{
+			using std::floor;
+			return map( []( const T value ) { return static_cast<T>( floor( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base ceil() const noexcept
+		{
+			using std::ceil;
+			return map( []( const T value ) { return static_cast<T>( ceil( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base round() const noexcept
+		{
+			using std::round;
+			return map( []( const T value ) { return static_cast<T>( round( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base trunc() const noexcept
+		{
+			using std::trunc;
+			return map( []( const T value ) { return static_cast<T>( trunc( value ) ); } );
+		}
+
+		// Exponent operations
+		[[nodiscard]] constexpr vec_base exp() const noexcept
+		{
+			using std::exp;
+			return map( []( const T value ) { return static_cast<T>( exp( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base log() const noexcept
+		{
+			using std::log;
+			return map( []( const T value ) { return static_cast<T>( log( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base log2() const noexcept
+		{
+			using std::log2;
+			return map( []( const T value ) { return static_cast<T>( log2( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base log10() const noexcept
+		{
+			using std::log10;
+			return map( []( const T value ) { return static_cast<T>( log10( value ) ); } );
+		}
+
+		// Power operations
+		[[nodiscard]] constexpr vec_base pow( const T exponent ) const noexcept
+		{
+			using std::pow;
+			return map( [ exponent ]( const T value ) { return static_cast<T>( pow( value, exponent ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base sqrt() const noexcept
+		{
+			using std::sqrt;
+			return map( []( const T value ) { return static_cast<T>( sqrt( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base cbrt() const noexcept
+		{
+			using std::cbrt;
+			return map( []( const T value ) { return static_cast<T>( cbrt( value ) ); } );
+		}
+
+		// Trigonmetric operations
+		[[nodiscard]] constexpr vec_base sin() const noexcept
+		{
+			using std::sin;
+			return map( []( const T value ) { return static_cast<T>( sin( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base cos() const noexcept
+		{
+			using std::cos;
+			return map( []( const T value ) { return static_cast<T>( cos( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base tan() const noexcept
+		{
+			using std::tan;
+			return map( []( const T value ) { return static_cast<T>( tan( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base asin() const noexcept
+		{
+			using std::asin;
+			return map( []( const T value ) { return static_cast<T>( asin( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base acos() const noexcept
+		{
+			using std::acos;
+			return map( []( const T value ) { return static_cast<T>( acos( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base atan() const noexcept
+		{
+			using std::atan;
+			return map( []( const T value ) { return static_cast<T>( atan( value ) ); } );
+		}
+
+		// Hyperbolic functions
+
+		[[nodiscard]] constexpr vec_base sinh() const noexcept
+		{
+			using std::sinh;
+			return map( []( const T value ) { return static_cast<T>( sinh( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base cosh() const noexcept
+		{
+			using std::cosh;
+			return map( []( const T value ) { return static_cast<T>( cosh( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base tanh() const noexcept
+		{
+			using std::tanh;
+			return map( []( const T value ) { return static_cast<T>( tanh( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base asinh() const noexcept
+		{
+			using std::asinh;
+			return map( []( const T value ) { return static_cast<T>( asinh( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base acosh() const noexcept
+		{
+			using std::acosh;
+			return map( []( const T value ) { return static_cast<T>( acosh( value ) ); } );
+		}
+
+		[[nodiscard]] constexpr vec_base atanh() const noexcept
+		{
+			using std::atanh;
+			return map( []( const T value ) { return static_cast<T>( atanh( value ) ); } );
+		}
+
+		// Geometry operations
+		[[nodiscard]] constexpr T norm_squared() const noexcept
+		{
+			return map( []( const T value ) { return value * value; } ).fold_left( std::plus<>{} );
+		}
+
+		[[nodiscard]] constexpr T norm() const noexcept
+		{
+			using std::sqrt;
+			return static_cast<T>( sqrt( norm_squared() ) );
+		}
+
+		[[nodiscard]] constexpr vec_base normalized() const noexcept
+		{
+			return *this / norm();
+		}
+
+		[[nodiscard]] constexpr T dot( const vec_base& other ) const noexcept
+		{
+			return ( *this * other ).fold_left( std::plus<>{} );
+		}
+
+		[[nodiscard]] constexpr T distance( const vec_base& other ) const noexcept
+		{
+			return ( *this - other ).norm();
+		}
+
+		[[nodiscard]] constexpr vec_base cross( const vec_base& other ) const noexcept
+			requires( N == 3 )
+		{
+			return { y() * other.z() - z() * other.y(),
+					 z() * other.x() - x() * other.z(),
+					 x() * other.y() - y() * other.x() };
+		}
+
+		[[nodiscard]] constexpr T angle( const vec_base& other ) const noexcept
+			requires( N == 3 )
+		{
+			using std::acos;
+			return acos( dot( other ) / ( norm() * other.norm() ) );
+		}
+
+		[[nodiscard]] constexpr auto operator<=>( const vec_base& other ) const noexcept = default;
+
+	private:
+		template <typename Func, std::size_t... Indices>
+		vec_base map_internal( Func func, std::index_sequence<Indices...> ) const noexcept
+		{
+			return { func( m_data[ Indices ] )... };
+		}
+
+		template <typename Func, std::size_t... Indices>
+		vec_base map_with_internal( Func func, const vec_base& other, std::index_sequence<Indices...> ) const noexcept
+		{
+			return { func( m_data[ Indices ], other.m_data[ Indices ] )... };
+		}
+
+		template <typename Func, std::size_t... Indices>
+		T fold_left_internal( Func func, T initial, std::index_sequence<Indices...> ) const noexcept
+		{
+			( ..., ( initial = func( initial, m_data[ Indices ] ) ) );
+			return initial;
+		}
+
+		template <typename Func, std::size_t... Indices>
+		T fold_right_internal( Func func, T initial, std::index_sequence<Indices...> ) const noexcept
+		{
+			( ( initial = func( initial, m_data[ Indices ] ) ), ... );
+			return initial;
+		}
+
+		std::array<T, N> m_data{};
+	};
+
+	template <typename T>
+	using vec2 = vec_base<T, 2>;
+
+	template <typename T>
+	using vec3 = vec_base<T, 3>;
+
+	template <typename T>
+	using vec4 = vec_base<T, 4>;
+
+	using vec2i = vec2<int>;
+	using vec3i = vec3<int>;
+	using vec4i = vec4<int>;
+
+	using vec2f = vec2<float>;
+	using vec3f = vec3<float>;
+	using vec4f = vec4<float>;
+
+	using vec2d = vec2<double>;
+	using vec3d = vec3<double>;
+	using vec4d = vec4<double>;
+
+#undef MCLO_VEC_ACCESSOR
+}
---
+++ tests/numeric_vector_tests.cpp
@@ -0,0 +1,254 @@
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "mclo/numeric/vector.hpp"
+
+#include "mclo/meta/type_list.hpp"
+
+namespace
+{
+	using test_types = mclo::meta::type_list<mclo::vec2i,
+											 mclo::vec3i,
+											 mclo::vec4i,
+											 mclo::vec2f,
+											 mclo::vec3f,
+											 mclo::vec4f,
+											 mclo::vec2d,
+											 mclo::vec3d,
+											 mclo::vec4d>;
+}
+
+TEMPLATE_LIST_TEST_CASE( "Vector shared constructors", "[vector][math]", test_types )
+{
+	using value_type = typename TestType::value_type;
+
+	static_assert( std::is_trivially_copyable_v<TestType> );
+	static_assert( std::is_trivially_destructible_v<TestType> );
+	static_assert( std::convertible_to<TestType, std::span<value_type, TestType::size()>> );
+	static_assert( std::convertible_to<const TestType, std::span<const value_type, TestType::size()>> );
+
+	SECTION( "Default constructor" )
+	{
+		TestType vec;
+		for ( auto i = 0; i < vec.size(); ++i )
+		{
+			CHECK( vec[ i ] == 0 );
+		}
+	}
+
+	SECTION( "Value constructor" )
+	{
+		TestType vec{ value_type( 1 ) };
+		for ( auto i = 0; i < vec.size(); ++i )
+		{
+			CHECK( vec[ i ] == 1 );
+		}
+	}
+
+	SECTION( "Copy constructor" )
+	{
+		TestType vec{ value_type( 1 ) };
+		TestType copy( vec );
+		for ( auto i = 0; i < vec.size(); ++i )
+		{
+			CHECK( copy[ i ] == 1 );
+		}
+	}
+
+	SECTION( "Move constructor" )
+	{
+		TestType vec{ value_type( 2 ) };
+		TestType move( std::move( vec ) );
+		for ( auto i = 0; i < move.size(); ++i )
+		{
+			CHECK( move[ i ] == 2 );
+		}
+	}
+
+	SECTION( "Span constructor" )
+	{
+		const auto arr = mclo::broadcast_array<TestType::size()>( value_type( 8 ) );
+		TestType vec{ arr };
+		for ( auto i = 0; i < vec.size(); ++i )
+		{
+			CHECK( vec[ i ] == arr[ i ] );
+		}
+	}
+}
+
+TEST_CASE( "Vector from values", "[vector][math]" )
+{
+	const mclo::vec2i vec2i( 1, 2 );
+	CHECK( vec2i[ 0 ] == 1 );
+	CHECK( vec2i[ 1 ] == 2 );
+	CHECK( vec2i.x() == 1 );
+	CHECK( vec2i.y() == 2 );
+
+	const mclo::vec3i vec3i( 1, 2, 3 );
+	CHECK( vec3i[ 0 ] == 1 );
+	CHECK( vec3i[ 1 ] == 2 );
+	CHECK( vec3i[ 2 ] == 3 );
+	CHECK( vec3i.x() == 1 );
+	CHECK( vec3i.y() == 2 );
+	CHECK( vec3i.z() == 3 );
+
+	const mclo::vec4i vec4i( 1, 2, 3, 4 );
+	CHECK( vec4i[ 0 ] == 1 );
+	CHECK( vec4i[ 1 ] == 2 );
+	CHECK( vec4i[ 2 ] == 3 );
+	CHECK( vec4i[ 3 ] == 4 );
+	CHECK( vec4i.x() == 1 );
+	CHECK( vec4i.y() == 2 );
+	CHECK( vec4i.z() == 3 );
+	CHECK( vec4i.w() == 4 );
+}
+
+TEST_CASE( "Vector writeable", "[vector][math]" )
+{
+	mclo::vec2i vec2i( 1, 2 );
+	vec2i[ 0 ] = 3;
+	vec2i[ 1 ] = 4;
+	CHECK( vec2i[ 0 ] == 3 );
+	CHECK( vec2i[ 1 ] == 4 );
+	CHECK( vec2i.x() == 3 );
+	CHECK( vec2i.y() == 4 );
+
+	mclo::vec3i vec3i( 1, 2, 3 );
+	vec3i[ 0 ] = 4;
+	vec3i[ 1 ] = 5;
+	vec3i[ 2 ] = 6;
+	CHECK( vec3i[ 0 ] == 4 );
+	CHECK( vec3i[ 1 ] == 5 );
+	CHECK( vec3i[ 2 ] == 6 );
+	CHECK( vec3i.x() == 4 );
+	CHECK( vec3i.y() == 5 );
+	CHECK( vec3i.z() == 6 );
+
+	mclo::vec4i vec4i( 1, 2, 3, 4 );
+	vec4i[ 0 ] = 5;
+	vec4i[ 1 ] = 6;
+	vec4i[ 2 ] = 7;
+	vec4i[ 3 ] = 8;
+	CHECK( vec4i[ 0 ] == 5 );
+	CHECK( vec4i[ 1 ] == 6 );
+	CHECK( vec4i[ 2 ] == 7 );
+	CHECK( vec4i[ 3 ] == 8 );
+	CHECK( vec4i.x() == 5 );
+	CHECK( vec4i.y() == 6 );
+	CHECK( vec4i.z() == 7 );
+	CHECK( vec4i.w() == 8 );
+}
+
+TEST_CASE( "Vector Addition", "[vector]" )
+{
+	mclo::vec2i v1( 1, 2 );
+	mclo::vec2i v2( 3, 4 );
+	mclo::vec2i expected( 4, 6 );
+
+	SECTION( "Operator+" )
+	{
+		mclo::vec2i result = v1 + v2;
+		REQUIRE( result == expected );
+	}
+
+	SECTION( "Operator+=" )
+	{
+		v1 += v2;
+		REQUIRE( v1 == expected );
+	}
+}
+
+TEST_CASE( "Vector Subtraction", "[vector]" )
+{
+	mclo::vec2i v1( 5, 6 );
+	mclo::vec2i v2( 3, 4 );
+	mclo::vec2i expected( 2, 2 );
+
+	SECTION( "Operator-" )
+	{
+		mclo::vec2i result = v1 - v2;
+		REQUIRE( result == expected );
+	}
+
+	SECTION( "Operator-=" )
+	{
+		v1 -= v2;
+		REQUIRE( v1 == expected );
+	}
+}
+
+TEST_CASE( "Vector Multiplication", "[vector]" )
+{
+	mclo::vec2i v1( 2, 3 );
+	mclo::vec2i v2( 4, 5 );
+	mclo::vec2i expected( 8, 15 );
+
+	SECTION( "Operator*" )
+	{
+		mclo::vec2i result = v1 * v2;
+		REQUIRE( result == expected );
+	}
+
+	SECTION( "Operator*=" )
+	{
+		v1 *= v2;
+		REQUIRE( v1 == expected );
+	}
+}
+
+TEST_CASE( "Vector Division", "[vector]" )
+{
+	mclo::vec2i v1( 10, 15 );
+	mclo::vec2i v2( 2, 3 );
+	mclo::vec2i expected( 5, 5 );
+
+	SECTION( "Operator/" )
+	{
+		mclo::vec2i result = v1 / v2;
+		REQUIRE( result == expected );
+	}
+
+	SECTION( "Operator/=" )
+	{
+		v1 /= v2;
+		REQUIRE( v1 == expected );
+	}
+}
+
+TEST_CASE( "Vector Dot Product", "[vector]" )
+{
+	mclo::vec2i v1( 1, 2 );
+	mclo::vec2i v2( 3, 4 );
+	const auto expected = 11;
+
+	const auto result = v1.dot( v2 );
+	REQUIRE( result == expected );
+}
+
+TEST_CASE( "Vector Cross Product", "[vector]" )
+{
+	mclo::vec3i v1( 1, 2, 3 );
+	mclo::vec3i v2( 4, 5, 6 );
+	mclo::vec3i expected( -3, 6, -3 );
+
+	const auto result = v1.cross( v2 );
+	REQUIRE( result == expected );
+}
+
+TEST_CASE( "Vector Norm", "[vector]" )
+{
+	mclo::vec2i v1( 3, 4 );
+	const auto expected = 5;
+
+	const auto result = v1.norm();
+	REQUIRE( result == expected );
+}
+
+TEST_CASE( "Vector Normalized", "[vector]" )
+{
+	mclo::vec2f v1( 3, 4.0f );
+	const mclo::vec2f expected( 0.6f, 0.8f );
+
+	const mclo::vec2f result = v1.normalized();
+	CHECK( result == expected );
+}
