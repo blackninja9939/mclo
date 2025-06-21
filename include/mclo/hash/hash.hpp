ef81209c09a2404c40d2351e44ab17b1b4b4b1bd
Matthew Clohessy <mclohessy@microsoft.com>
2025-06-21 17:29:03 +0200

    Fix some shadowing

--- include/mclo/hash/hash.hpp
+++ include/mclo/hash/hash.hpp
@@ -25,18 +25,18 @@ namespace mclo
 	template <hasher Hasher = mclo::default_hasher, hashable_with<Hasher> T>
 	std::size_t hash_object( const T& value ) noexcept
 	{
-		Hasher hasher;
-		hash_append( hasher, value );
-		return hasher.finish();
+		Hasher h;
+		hash_append( h, value );
+		return h.finish();
 	}
 
 	template <hasher Hasher = mclo::default_hasher, std::ranges::forward_range Range>
 		requires( hashable_with<std::ranges::range_value_t<Range>, Hasher> )
 	std::size_t hash_range( Range&& range ) noexcept
 	{
-		Hasher hasher;
-		hash_append_range( hasher, std::forward<Range>( range ) );
-		return hasher.finish();
+		Hasher h;
+		hash_append_range( h, std::forward<Range>( range ) );
+		return h.finish();
 	}
 
 	template <typename T>
