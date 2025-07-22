2205e00898b1465eb069ed9a67de97b39ceaae0f
blackninja9939 <blackninja9939@users.noreply.github.com>
2025-07-22 23:08:47 +0200

    Add the missing reset condolidate

--- source/allocator/arena_allocator.cpp
+++ source/allocator/arena_allocator.cpp
@@ -52,6 +52,24 @@ namespace mclo
 		m_current = m_current_chunk->begin();
 	}
 
+	void memory_arena::reset_consolidate()
+	{
+		std::size_t toal_size = 0;
+
+		chunk* head = m_head;
+		while ( head )
+		{
+			toal_size += head->m_size;
+			chunk* const next = head->m_next;
+			::operator delete( head, std::align_val_t{ alignof( chunk ) } );
+			head = next;
+		}
+
+		m_head = allocate_chunk( toal_size );
+		m_current_chunk = m_head;
+		m_current = m_head->begin();
+	}
+
 	void memory_arena::release() noexcept
 	{
 		chunk* head = m_head;
