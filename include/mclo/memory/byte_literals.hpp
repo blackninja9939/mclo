c5f04a9edfd0d04c205836ec77de74ac8346cf74
blackninja9939 <blackninja9939@users.noreply.github.com>
2025-11-12 19:21:22 +0100

    Typos galore

--- include/mclo/memory/byte_literals.hpp
+++ include/mclo/memory/byte_literals.hpp
@@ -8,27 +8,27 @@ namespace mclo
 	{
 		inline namespace byte_literals
 		{
-			[[nodsicard]] constexpr std::size_t operator""_KiB( unsigned long long int x ) noexcept
+			[[nodiscard]] constexpr std::size_t operator""_KiB( unsigned long long int x ) noexcept
 			{
 				return 1024ULL * x;
 			}
 
-			[[nodsicard]] constexpr std::size_t operator""_MiB( unsigned long long int x ) noexcept
+			[[nodiscard]] constexpr std::size_t operator""_MiB( unsigned long long int x ) noexcept
 			{
 				return 1024_KiB * x;
 			}
 
-			[[nodsicard]] constexpr std::size_t operator""_GiB( unsigned long long int x ) noexcept
+			[[nodiscard]] constexpr std::size_t operator""_GiB( unsigned long long int x ) noexcept
 			{
 				return 1024_MiB * x;
 			}
 
-			[[nodsicard]] constexpr std::size_t operator""_TiB( unsigned long long int x ) noexcept
+			[[nodiscard]] constexpr std::size_t operator""_TiB( unsigned long long int x ) noexcept
 			{
 				return 1024_GiB * x;
 			}
 
-			[[nodsicard]] constexpr std::size_t operator""_PiB( unsigned long long int x ) noexcept
+			[[nodiscard]] constexpr std::size_t operator""_PiB( unsigned long long int x ) noexcept
 			{
 				return 1024_TiB * x;
 			}
