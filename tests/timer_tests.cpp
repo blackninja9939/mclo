3fa79a21b3c475be24c193fc662dd3ef55beb91c
blackninja9939 <blackninja9939@users.noreply.github.com>
2025-11-17 17:32:44 +0100

    Fix test in optimized mode

--- tests/timer_tests.cpp
+++ tests/timer_tests.cpp
@@ -3,22 +3,30 @@
 
 #include "mclo/utility/timer.hpp"
 
+#include <thread>
+
 TEST_CASE( "timer elapsed returns delta", "[timer]" )
 {
 	mclo::timer<> timer;
 	using duration = mclo::timer<>::duration;
+	const duration sleep_time( 2 );
+	std::this_thread::sleep_for( sleep_time );
 
 	const duration delta = timer.elapsed();
 
-	CHECK( delta > duration() );
+	CHECK( delta >= sleep_time );
 }
 
 TEST_CASE( "timer tick returns detla and sets point", "[timer]" )
 {
 	mclo::timer<> timer;
 	using duration = mclo::timer<>::duration;
+	const duration sleep_time( 2 );
+	std::this_thread::sleep_for( sleep_time );
 
 	const duration delta = timer.tick();
+	const duration elapsed = timer.elapsed();
 
-	CHECK( delta > duration() );
+	CHECK( delta >= sleep_time );
+	CHECK( elapsed < delta );
 }
