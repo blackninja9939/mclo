3dc0edb8126e82d54b25248ff66edd2cfeeb0c41
mclohessy <mclohessy@microsoft.com>
2026-06-15 14:36:14 +0200

    Optimize ref count decrement
    It provides nicer perf on ARM etc where we can avoid the expensive acquire unless we are actually the final one running deletion.

--- include/mclo/memory/intrusive_ref_counter.hpp
+++ include/mclo/memory/intrusive_ref_counter.hpp
@@ -31,10 +31,11 @@ namespace mclo
 
 		friend void intrusive_ptr_release_ref( const intrusive_ref_counter<Derived>* ptr ) noexcept
 		{
-			const std::size_t old = ptr->m_counter.fetch_sub( 1, std::memory_order_acq_rel );
+			const std::size_t old = ptr->m_counter.fetch_sub( 1, std::memory_order_release );
 			DEBUG_ASSERT( old != 0, "Reference count underflow in intrusive_ref_counter" );
 			if ( old == 1 ) // Was last reference
 			{
+				std::atomic_thread_fence( std::memory_order_acquire );
 				delete static_cast<const Derived*>( ptr );
 			}
 		}
