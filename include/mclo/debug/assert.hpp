b93d289526819cdef692414e4bda022762dd34d9
mclohessy <mclohessy@microsoft.com>
2026-06-29 11:24:39 +0100

    Document asserts

--- include/mclo/debug/assert.hpp
+++ include/mclo/debug/assert.hpp
@@ -76,24 +76,10 @@ namespace mclo::detail
 	}
 }
 
-#ifndef MCLO_DEBUG_ASSERT
 #ifdef NDEBUG
-#define MCLO_DEBUG_ASSERT( EXPR, ... ) ( ( void )0 )
+#define MCLO_DETAIL_DEBUG_ASSERT( EXPR, ... ) ( ( void )0 )
 #else
-#define MCLO_DEBUG_ASSERT( EXPR, ... )                                                                                 \
-	do                                                                                                                 \
-	{                                                                                                                  \
-		if ( !( EXPR ) ) [[unlikely]]                                                                                  \
-		{                                                                                                              \
-			mclo::detail::assert_failed( #EXPR, MCLO_PRETTY_FUNCTION, __FILE__, __LINE__ __VA_OPT__(, __VA_ARGS__ ) ); \
-		}                                                                                                              \
-	}                                                                                                                  \
-	while ( 0 )
-#endif
-#endif
-
-#ifndef MCLO_ASSERT
-#define MCLO_ASSERT( EXPR, ... )                                                                                       \
+#define MCLO_DETAIL_DEBUG_ASSERT( EXPR, ... )                                                                          \
 	do                                                                                                                 \
 	{                                                                                                                  \
 		if ( !( EXPR ) ) [[unlikely]]                                                                                  \
@@ -104,22 +90,31 @@ namespace mclo::detail
 	while ( 0 )
 #endif
 
-#ifndef MCLO_ASSUME
+#define MCLO_DETAIL_ASSERT( EXPR, ... )                                                                                \
+	do                                                                                                                 \
+	{                                                                                                                  \
+		if ( !( EXPR ) ) [[unlikely]]                                                                                  \
+		{                                                                                                              \
+			mclo::detail::assert_failed( #EXPR, MCLO_PRETTY_FUNCTION, __FILE__, __LINE__ __VA_OPT__(, __VA_ARGS__ ) ); \
+		}                                                                                                              \
+	}                                                                                                                  \
+	while ( 0 )
+
 #ifdef NDEBUG
 // Prefer the cross platform attribute when available, otherwise fall back to platform specific extensions
 #if __has_cpp_attribute( assume )
-#define MCLO_ASSUME( EXPR, ... ) [[assume( EXPR )]]
+#define MCLO_DETAIL_ASSUME( EXPR, ... ) [[assume( EXPR )]]
 #elif defined( MCLO_COMPILER_CLANG )
-#define MCLO_ASSUME( EXPR, ... ) __builtin_assume( EXPR )
+#define MCLO_DETAIL_ASSUME( EXPR, ... ) __builtin_assume( EXPR )
 #elif defined( MCLO_COMPILER_GCC )
-#define MCLO_ASSUME( EXPR, ... ) __attribute__( ( assume( EXPR ) ) )
+#define MCLO_DETAIL_ASSUME( EXPR, ... ) __attribute__( ( assume( EXPR ) ) )
 #elif defined( MCLO_COMPILER_MSVC )
-#define MCLO_ASSUME( EXPR, ... ) __assume( EXPR )
+#define MCLO_DETAIL_ASSUME( EXPR, ... ) __assume( EXPR )
 #else
-#define MCLO_ASSUME( EXPR, ... )
+#define MCLO_DETAIL_ASSUME( EXPR, ... )
 #endif
 #else
-#define MCLO_ASSUME( EXPR, ... )                                                                                       \
+#define MCLO_DETAIL_ASSUME( EXPR, ... )                                                                                \
 	do                                                                                                                 \
 	{                                                                                                                  \
 		if ( !( EXPR ) ) [[unlikely]]                                                                                  \
@@ -129,22 +124,18 @@ namespace mclo::detail
 	}                                                                                                                  \
 	while ( 0 )
 #endif
-#endif
 
-#ifndef MCLO_PANIC
-#define MCLO_PANIC( ... )                                                                                              \
+#define MCLO_DETAIL_PANIC( ... )                                                                                       \
 	do                                                                                                                 \
 	{                                                                                                                  \
 		mclo::detail::assert_failed( "PANIC", MCLO_PRETTY_FUNCTION, __FILE__, __LINE__, __VA_ARGS__ );                 \
 	}                                                                                                                  \
 	while ( 0 )
-#endif
 
-#ifndef MCLO_UNREACHABLE
 #ifdef NDEBUG
-#define MCLO_UNREACHABLE( ... ) mclo::unreachable()
+#define MCLO_DETAIL_UNREACHABLE( ... ) mclo::unreachable()
 #else
-#define MCLO_UNREACHABLE( ... )                                                                                        \
+#define MCLO_DETAIL_UNREACHABLE( ... )                                                                                 \
 	do                                                                                                                 \
 	{                                                                                                                  \
 		mclo::detail::assert_failed( "UNREACHABLE", MCLO_PRETTY_FUNCTION, __FILE__, __LINE__, __VA_ARGS__ );           \
@@ -152,4 +143,43 @@ namespace mclo::detail
 	while ( 0 )
 #endif
 
+/// @brief Checks a programmer-error precondition in debug builds; compiled out entirely under @c NDEBUG.
+/// @details Use for internal invariants that should be caught and fixed before shipping. The condition is not even
+/// evaluated in release, so it must be free of required side effects.
+/// @param EXPR The condition that must hold.
+/// @param ... Optional message string followed by optional variables whose values are printed.
+#ifndef MCLO_DEBUG_ASSERT
+#define MCLO_DEBUG_ASSERT( EXPR, ... ) MCLO_DETAIL_DEBUG_ASSERT( EXPR __VA_OPT__(, __VA_ARGS__ ) )
+#endif
+
+/// @brief Checks a non-recoverable invariant in both debug and release builds.
+/// @details Use for invariants that cannot be recovered from but could occur due to invalid user input. Unlike
+/// @ref MCLO_DEBUG_ASSERT the condition is always evaluated and checked.
+/// @param EXPR The condition that must hold.
+/// @param ... Optional message string followed by optional variables whose values are printed.
+#ifndef MCLO_ASSERT
+#define MCLO_ASSERT( EXPR, ... ) MCLO_DETAIL_ASSERT( EXPR __VA_OPT__(, __VA_ARGS__ ) )
+#endif
+
+/// @brief Checks a condition in debug builds; in release acts as an optimisation hint that the condition is true.
+/// @details Use for conditions that are impossible by design. In release builds a false condition is undefined
+/// behaviour rather than a checked failure, so the condition must be free of required side effects.
+/// @param EXPR The condition that is assumed to hold.
+/// @param ... Optional message string followed by optional variables whose values are printed.
+#ifndef MCLO_ASSUME
+#define MCLO_ASSUME( EXPR, ... ) MCLO_DETAIL_ASSUME( EXPR __VA_OPT__(, __VA_ARGS__ ) )
+#endif
+
+/// @brief Unconditionally fails in both debug and release builds; an unconditional @ref MCLO_ASSERT.
+/// @details Use for code paths that should never run. A message is required.
+/// @param ... The message string followed by optional variables whose values are printed.
+#ifndef MCLO_PANIC
+#define MCLO_PANIC( ... ) MCLO_DETAIL_PANIC( __VA_ARGS__ )
+#endif
+
+/// @brief Fails in debug builds; in release acts as an optimisation hint that the path is unreachable.
+/// @details Use for code paths that should be unreachable; an unconditional @ref MCLO_ASSUME. A message is required.
+/// @param ... The message string followed by optional variables whose values are printed.
+#ifndef MCLO_UNREACHABLE
+#define MCLO_UNREACHABLE( ... ) MCLO_DETAIL_UNREACHABLE( __VA_ARGS__ )
 #endif
